90e2a04a57da23594841274106e44a873361ea92
oreon-core <oreon.core@gmail.com>
2025-12-28 22:48:16 +0200

    1.0.0 realised main functions for 1.21.9

--- fabric/src/client/java/com/compactinfo/Keybinds.java
+++ fabric/src/client/java/com/compactinfo/Keybinds.java
@@ -5,35 +5,34 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.text.Text;
-
+import net.minecraft.util.Identifier;
 public final class Keybinds {
     private static KeyBinding toggleHudKey;
     private static KeyBinding openHudConfigKey;
 
-    public static void register() {
 
+    private static final KeyBinding.Category COMPACTINFO_CATEGORY =
+            KeyBinding.Category.create(Identifier.of("compactinfo", "keybind_category"));
+    public static void register() {
         toggleHudKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.compactinfo.toggle_hud",
                 InputUtil.Type.KEYSYM,
                 InputUtil.UNKNOWN_KEY.getCode(),
-                "category.compactinfo"
+                COMPACTINFO_CATEGORY
         ));
-
 
         openHudConfigKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.compactinfo.open_hud_config",
                 InputUtil.Type.KEYSYM,
                 InputUtil.UNKNOWN_KEY.getCode(),
-                "category.compactinfo"
+                COMPACTINFO_CATEGORY
         ));
 
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
 
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (toggleHudKey.wasPressed()) {
                 HudOverlay.toggle();
             }
-
 
             while (openHudConfigKey.wasPressed()) {
                 MinecraftClient.getInstance().setScreen(
